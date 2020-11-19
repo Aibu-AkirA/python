6d560ddcfe3a1f5117befde266592d23a7e40a31
Akira Aibu <wkdgml369@gmail.com>
2020-11-19 16:01:14 +0900

    2020/11/19(school)commit

---
+++ 99. 기타 자료/KeyboardValue/test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import msvcrt
+import sys
+
+while True:
+    print("入力してください。")
+    ch = msvcrt.getch()
+    ch2 = ord(ch)
+    print(ch)
+    print(ch2)
+
+
+# In[ ]:
+
+
+
+
